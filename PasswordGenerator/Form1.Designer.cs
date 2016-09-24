a26a99a3d02313f97808df5ed664a81758083c91
William Maillard <william@zeero.ch>
2016-09-24 18:09:41 +0200

    Inital commit

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PasswordGenerator
+{
+    partial class PasswordGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordGenerator));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 89);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(482, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate and Copy in clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 89);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PasswordGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(482, 143);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "PasswordGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AD Password Generator";
+            this.Load += new System.EventHandler(this.PasswordGenerator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class PasswordGenerator : Form
+    {
+        public PasswordGenerator()
+        {
+            InitializeComponent();
+        }
+
+        static class RandomLetter
+        {
+            static Random _random = new Random();
+            public static char GetLetter()
+            {
+                // This method returns a random lowercase letter.
+                // ... Between 'a' and 'z' inclusize.
+                int num = _random.Next(0, 26); // Zero to 25
+                char let = (char)('a' + num);
+                return let;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = "";
+            string[] chars;
+            chars = new string[8];
+            Random r = new Random();
+
+            // Fixer la taille du mot de passe
+            int taille = 8;
+
+            // Fixer le nombre de lettres avant les chiffres
+            int letters = 4;
+
+            // Le nombre de chiffres se calcule automatiquement en fonction
+            // de la longueur du mot de passe et du nombre de lettres
+            int numbers = taille - letters;
+
+            // Incrémentation tableau "chars"
+            int j = 0;
+
+            // Génération des lettres
+            for(int i = 0; i < letters; i++)
+            {
+                chars[j] = RandomLetter.GetLetter().ToString();
+                
+                // Première lettre en majuscule
+                if(i == 0)
+                {
+                    chars[j] = chars[j].ToUpper();
+                }
+                else
+                {
+                    // On ne veut pas deux fois la même lettre à la suite
+                    while (chars[j].Equals(chars[j-1].ToLower()))
+                    {
+                        //Console.Write(chars[j - 1] + chars[j]);
+                        chars[j] = RandomLetter.GetLetter().ToString();
+                        //Console.WriteLine(" -> " + chars[j - 1] + chars[j]);
+                    }
+                }
+
+                j++;
+            }
+
+            // Génération des chiffres
+            for (int i = 0; i < numbers; i++)
+            {
+                chars[j] = r.Next(0, 10).ToString();
+
+                if (j > letters)
+                {
+                    // On ne veut pas deux fois le même chiffre à la suite
+                    while (chars[j].Equals(chars[j - 1]))
+                    {
+                        //Console.Write(chars[j - 1] + chars[j]);
+                        chars[j] = r.Next(0, 10).ToString();
+                        //Console.WriteLine(" -> " + chars[j - 1] + chars[j]);
+                    }
+                }
+
+                j++;
+            }
+
+            // Transformer le tableau "chars" en un string "password"
+            for (int i = 0; i < 8; i++)
+            {
+                password = password + chars[i];
+            }
+
+            // Copier le mot de passe dans le presse papier
+            Clipboard.SetText(password);
+
+            // Afficher le mot de passe dans la fenêtre
+            textBox1.Text = password;
+        }
+
+        private void PasswordGenerator_Load(object sender, EventArgs e)
+        {
+            // Afficher ce texte dans le champ mot de passe
+            textBox1.Text = "-------------";
+        }
+    }
+}
